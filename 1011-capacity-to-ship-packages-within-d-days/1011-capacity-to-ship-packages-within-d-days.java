c0d9a432e4b661680eca314b4fee9cd9303f7f9f
Kareena Patidar <kareenapatidar329@gmail.com>
2026-07-20 18:48:16 +0530

    Time: 9 ms (98.71%), Space: 50.2 MB (37.91%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,33 +1,40 @@
+//tc = O(n log(sum))  //sc=O(1)
+
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
         int low = 0;
         int high = 0;
-        for(int w : weights){
-            low = Math.max(low,w);
-            high += w;
+
+        for(int weight : weights){
+            low = Math.max(low,weight);
+            high += weight;
         }
-        while(low<high){
-            int mid = low+(high - low)/2;
-            if(canShip(weights,days,mid)){
-                high = mid;
+        int ans = high;
+
+        while(low <= high){
+            int mid = low + (high - low) /2;
+            if(canShip(weights, days, mid)){
+                ans = mid;
+                high = mid -1;
             }else{
-                low = mid + 1;
+                low = mid +1;
             }
         }
-        return low;
+        return ans;
     }
-    private boolean canShip(int[]weights,int days,int capacity){
+    private boolean canShip (int [] weights, int days, int capacity){
         int usedDays = 1;
         int currentLoad = 0;
 
-        for(int w:weights){
-            if(currentLoad + w > capacity){
-                usedDays++;
-                currentLoad = 0;
-
+        for(int weight : weights){
+            if(currentLoad + weight <= capacity){
+                currentLoad += weight;
+            }else{
+            usedDays++;
+            currentLoad = weight;
             }
-            currentLoad += w;
         }
-        return usedDays <= days;
+    
+    return usedDays <= days;
     }
 }
