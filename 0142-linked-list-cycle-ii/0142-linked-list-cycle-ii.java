b9433994c558aa6f69fd2d4782023420e63b9b1e
Kareena Patidar <kareenapatidar329@gmail.com>
2026-08-12 21:23:34 +0530

    Time: 0 ms (100%), Space: 46.7 MB (50.51%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -9,29 +9,24 @@
  *     }
  * }
  */
-
-//tc = O(n) //sc = O(1)
 public class Solution {
-    public ListNode detectCycle(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-        while(fast != null && fast.next != null){
-            slow = slow.next;
-            fast = fast.next.next;
-
-            if(slow == fast){
-
-                ListNode p1 = head;
-                ListNode p2 = slow;
-
-                while( p1 != p2){
-                    p1 = p1.next;
-                    p2 = p2.next;
-                }
-            
-            return p1;
-            }
-        }
-        return null;
-    }
+   public ListNode detectCycle(ListNode head) { 
+    ListNode slow = head; 
+    ListNode fast = head;
+     // Phase 1: Find meeting point 
+   while (fast != null && fast.next != null) {
+     slow = slow.next; 
+     fast = fast.next.next;
+      if (slow == fast) { // Phase 2: Find cycle start 
+      ListNode p1 = head;
+      ListNode p2 = slow;
+       while (p1 != p2) {
+         p1 = p1.next; 
+         p2 = p2.next; 
+} 
+         return p1; 
+    } 
+         }
+          return null;
+  }
 }
