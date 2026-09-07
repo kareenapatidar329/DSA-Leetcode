b8473e92ab02a138c05a3484bc1e22b0128139e4
Kareena Patidar <kareenapatidar329@gmail.com>
2026-09-07 17:55:22 +0530

    Time: 6 ms (94.11%), Space: 44.9 MB (60.4%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.java
@@ -0,0 +1,24 @@
+//tc = O(n log n * k)  //sc = (n)
+
+class Solution {
+    public String largestNumber(int[] nums) {
+        String[] arr = new String[nums.length];
+        for(int i=0; i<nums.length; i++){
+            arr[i] = String.valueOf(nums[i]);
+        }
+        Arrays.sort(arr,(a,b) -> {
+            String ab = a + b;
+            String ba = b + a;
+
+            return ba.compareTo(ab);
+        });
+        if(arr[0].equals("0")){
+            return "0";
+        }
+        StringBuilder ans = new StringBuilder();
+        for(String s : arr){
+            ans.append(s);
+        }
+        return ans.toString();
+    }
+}
