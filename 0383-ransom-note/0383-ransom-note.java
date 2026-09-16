61a7a0276621094c5fd3b4754de2a4300203b4ba
Kareena Patidar <kareenapatidar329@gmail.com>
2026-09-16 23:37:32 +0530

    Time: 16 ms (19.21%), Space: 47.4 MB (7.33%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,6 +1,5 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-
         HashMap<Character, Integer> map = new HashMap<>();
 
         // Count characters in magazine
